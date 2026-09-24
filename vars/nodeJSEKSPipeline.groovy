c604439f65639980c9d0df54a2996c9c4fdd15ed
e-shekharreddy <shekharreddy.eanala.com>
2026-09-24 14:26:26 +0530

    shared-library

--- vars/nodeJSEKSPipeline.groovy
+++ vars/nodeJSEKSPipeline.groovy
@@ -28,7 +28,7 @@ def call(Map configMap){
                         // Access fields directly
                         appVersion = packageJson.version
                         echo "Building version ${appVersion}"
-                        sh 'printenv | sort'
+                        // sh 'printenv | sort'
                     }
                 }
             }
